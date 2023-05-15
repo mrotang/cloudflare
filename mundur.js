ccf1fa945cee312556e593f50485411385fae23a
mrotang <65051182+mrotang@users.noreply.github.com>
2023-05-15 17:38:19 +0700

    Update mundur.js

--- mundur.js
+++ mundur.js
@@ -1,13 +1,27 @@
-var countdownButton = document.getElementById('countdown-button');
+<script type="text/javascript">
+            let count = <?php echo $timer_btn; ?>;
+            let counter = setInterval(timer, 1000);
+            function timer() {
+                count = count - 1;
+                if (count <= 0) {
+                    document.getElementById('elpe-wait1').style.display = 'none';
+                    document.getElementById('elpe-generate').style.display = 'block';
+                    clearInterval(counter);
+                    return;
+                }
+                document.getElementById("elpe-time").innerHTML = count;
+            }
 
-  var countdown = 10;
-  var countdownInterval = setInterval(function() {
-    countdown--;
-    countdownButton.textContent = 'Hitung Mundur ' + countdown;
+            function elpegenerate() {
+                document.getElementById('elpe').focus();
+                document.getElementById('elpe-link').style.display = 'none';
+                document.getElementById('elpe-wait2').style.display = 'block';
 
-    if (countdown === 0) {
-      countdownButton.removeAttribute('disabled');
-      countdownButton.textContent = 'Klik Sekarang!';
-      clearInterval(countdownInterval);
-    }
-  }, 1000);
+                setInterval(function () {
+                    document.getElementById('elpe-wait2').style.display = 'none';
+                }, 30000);
+                setInterval(function () {
+                    document.getElementById('elpe-link').style.display = 'block';
+                }, 2000);
+            }
+</script>

efcc9c0e61adb816e540d34ad35c3f2790ae1b11
mrotang <65051182+mrotang@users.noreply.github.com>
2023-05-15 17:43:07 +0700

    Update template2.js

--- template2.js
+++ template2.js
@@ -260,6 +260,33 @@ setTimeout(function() {
   document.body.style.overflow = "auto";
 }, 5000);
 </script>
+<script>
+           let count = <?php echo $timer_btn; ?>;
+            let counter = setInterval(timer, 1000);
+            function timer() {
+                count = count - 1;
+                if (count <= 0) {
+                    document.getElementById('elpe-wait1').style.display = 'none';
+                    document.getElementById('elpe-generate').style.display = 'block';
+                    clearInterval(counter);
+                    return;
+                }
+                document.getElementById("elpe-time").innerHTML = count;
+            }
+
+            function elpegenerate() {
+                document.getElementById('elpe').focus();
+                document.getElementById('elpe-link').style.display = 'none';
+                document.getElementById('elpe-wait2').style.display = 'block';
+
+                setInterval(function () {
+                    document.getElementById('elpe-wait2').style.display = 'none';
+                }, 30000);
+                setInterval(function () {
+                    document.getElementById('elpe-link').style.display = 'block';
+                }, 2000);
+            }
+</script>
 <div class="cfmonitor"><center><script type="text/javascript">
         $(document).ready(function() {$('img#closed').click(function(){$('#btm_banner').hide(90);});});
 </script>

40b2142fb10bc8b15bd4615024034a29575f9712
mrotang <65051182+mrotang@users.noreply.github.com>
2023-05-15 17:21:55 +0700

    Create kans.js

---
+++ kans.js
@@ -0,0 +1,1 @@
+var uri = window.location.toString();if (uri.indexOf(&quot;?br21r=2&quot;,&quot;?br21r=2&quot;) &gt; 0) {var clean_uri = uri.substring(0, uri.indexOf(&quot;?br21r=2&quot;));window.history.replaceState({}, document.title, clean_uri);}

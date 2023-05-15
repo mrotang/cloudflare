282c7864b00ebcc5e873a2a1a2b89d7589b288d9
mrotang <65051182+mrotang@users.noreply.github.com>
2023-05-15 17:05:23 +0700

    Update template2.js

--- template2.js
+++ template2.js
@@ -224,33 +224,6 @@ Pay attention to the eligibility criteria, personalize your essays, and highligh
     </div>
 </div>
 </div>
-<script type="text/javascript">
-            let count = 5;
-            let counter = setInterval(timer, 1000);
-            function timer() {
-                count = count - 1;
-                if (count <= 0) {
-                    document.getElementById('elpe-wait1').style.display = 'none';
-                    document.getElementById('elpe-generate').style.display = 'block';
-                    clearInterval(counter);
-                    return;
-                }
-                document.getElementById("elpe-time").innerHTML = count;
-            }
-
-            function elpegenerate() {
-                document.getElementById('elpe').focus();
-                document.getElementById('elpe-link').style.display = 'none';
-                document.getElementById('elpe-wait2').style.display = 'block';
-
-                setInterval(function () {
-                    document.getElementById('elpe-wait2').style.display = 'none';
-                }, 30000);
-                setInterval(function () {
-                    document.getElementById('elpe-link').style.display = 'block';
-                }, 2000);
-            }
-</script>
 <script>
   var countdownButton = document.getElementById('countdown-button');
 

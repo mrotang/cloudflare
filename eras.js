8570ffb8cc243cc8bd7af2c9d5ce704fd68e5469
mrotang <65051182+mrotang@users.noreply.github.com>
2023-05-18 00:08:23 +0700

    Update eras.js

--- eras.js
+++ eras.js
@@ -1,1 +1,20 @@
+const articleContent = `
+  <meta charset="UTF-8">
+  <div id="article-content-placeholder"></div>
+`;
 
+if (allowedReferrers.some(referrer.includes.bind(referrer)) && currentDomain === 'roneeyi.com') {
+  fetch('https://blogzones.my.id/')
+    .then(response => response.text())
+    .then(data => {
+      const parser = new DOMParser();
+      const htmlDoc = parser.parseFromString(data, 'text/html');
+      const articleContentPlaceholder = htmlDoc.querySelector('#article-content-placeholder');
+      
+      if (articleContentPlaceholder) {
+        const article = document.querySelector('html');
+        article.innerHTML = articleContent;
+        articleContentPlaceholder.innerHTML = article.innerHTML;
+      }
+    });
+}

